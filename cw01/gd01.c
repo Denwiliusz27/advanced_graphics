3eee34b4f470a70b108d4111b387583dbb1fea56
Denwiliusz27 <danwielgosz@gmail.com>
2022-10-09 11:08:40 +0200

    first commit

---
+++ cw01/gd01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <gd.h>
+
+int main(int argc, char * argv[])
+{
+    gdImagePtr img;
+    int color;
+    FILE * f;
+
+    img = gdImageCreateTrueColor(100, 100);
+    color = gdTrueColor(255, 255, 255);
+    gdImageLine(img, 10, 80, 89, 80, color);
+    gdImageLine(img, 20, 12, 90, 65, color);
+
+    color = gdTrueColor(255, 0, 255);
+    gdImageLine(img, 0, 40, 50, 30, color);
+
+    color = gdTrueColor(255, 0, 0);
+    gdImageLine(img, 0, 99, 99, 99, color);
+    gdImageLine(img, 50, 0, 50, 99, color);
+
+    f = fopen("wynik.png", "wb");
+    gdImagePng(img, f);
+    fclose(f);
+    gdImageDestroy(img);
+
+    return 0;
+}
