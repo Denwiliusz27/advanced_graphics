48f831338cc954670965a672813e4667a1146793
Denwiliusz27 <danwielgosz@gmail.com>
2023-01-24 22:43:44 +0100

    frog is jumping

--- projekt_3D/ZGK_1.cpp
+++ projekt_3D/ZGK_1.cpp
@@ -21,19 +21,22 @@ USE_GRAPHICSWINDOW()
 
 using namespace std;
 
+bool win = false;
+
 
 class RiverObject {
 public:
     RiverObject(osg::MatrixTransform* node, int direction, double size)
-        : _node(node), _direction(new int(direction)), size(size) {}
+        : _node(node), direction(direction), size(size) {}
 
     osg::MatrixTransform* getMatrix();
-    int* getDirection();
+    int getDirection();
     double getSize();
+    void setDirection(int dir);
 
 private:
     osg::ref_ptr<osg::MatrixTransform> _node;
-    int* _direction;
+    int direction;
     double size;
 };
 
@@ -41,13 +44,16 @@ osg::MatrixTransform* RiverObject::getMatrix() {
     return _node;
 }
 
-int* RiverObject::getDirection() {
-    return _direction;
+int RiverObject::getDirection() {
+    return direction;
 }
-
 
 double RiverObject::getSize() {
     return size;
+}
+
+void RiverObject::setDirection(int dir) {
+    direction = dir;
 }
 
 
@@ -57,72 +63,86 @@ double RiverObject::getSize() {
 class ModelController : public osgGA::GUIEventHandler
 {
 public:
-    ModelController(osg::MatrixTransform* node, vector<osg::MatrixTransform*> matrixes)
+    ModelController(osg::MatrixTransform* node, vector<RiverObject*> matrixes)
         : _model(node), obj_matrixes(matrixes) {}
 
     virtual bool handle(const osgGA::GUIEventAdapter& ea,
         osgGA::GUIActionAdapter& aa) override;
 protected:
     osg::ref_ptr<osg::MatrixTransform> _model;
-    vector<osg::MatrixTransform*> obj_matrixes;
+    vector<RiverObject*> obj_matrixes;
 };
 
 
 bool ModelController::handle(const osgGA::GUIEventAdapter& ea,
     osgGA::GUIActionAdapter& aa)
 {
+    if (ea.getEventType() != osgGA::GUIEventAdapter::FRAME) return false;
+
     if (!_model) return false;
     osg::Matrix frog_matrix = _model->getMatrix();
     osg::Vec3 frog_pos = frog_matrix.getTrans();
-    switch (ea.getEventType()) {
-    case osgGA::GUIEventAdapter::KEYDOWN:
-        switch (ea.getKey()) {
-        case 'a': case 'A':
+    
+
+    if (!win) {
+        if (Input::GetKeyDown('a')) {
             if (frog_pos.x() <= 10.0 && frog_pos.x() >= -8.0) {
                 frog_matrix *= osg::Matrix::translate(-2.0, 0.0, 0.0);
+                cout << "a" << endl;
             }
-            break;
-        case 'd': case 'D':
+        }
+        else if (Input::GetKeyDown('d')) {
             if (frog_pos.x() <= 8.0 && frog_pos.x() >= -10.0) {
                 frog_matrix *= osg::Matrix::translate(2.0, 0.0, 0.0);
+                cout << "d" << endl;
             }
-            break;
-        case 'w': case 'W':
-            frog_matrix *= osg::Matrix::translate(0.0, 2.0, 0.0);
-            break;
-        default:
-            break;
         }
-        _model->setMatrix(frog_matrix);
-        break;
-    default:
-        break;
+        else if (Input::GetKeyDown('w')) {
+            if (frog_pos.y() < 10.0) {
+                cout << "FROGG: (" << frog_pos.y() << endl;
+                frog_matrix *= osg::Matrix::translate(0.0, 2.0, 0.0);
+                cout << "w" << endl;
+            }
+            else {
+                win = true;
+            }
+        }
     }
+    
+    _model->setMatrix(frog_matrix);
+    frog_pos = frog_matrix.getTrans();
 
-    cout << "FROGG: (" << frog_pos.x() << ", " << frog_pos.y() << ", " << frog_pos.z() << ")" << endl;
+  
+    //cout << "FROGG: (" << frog_pos.x() << ", " << frog_pos.y() << ", " << frog_pos.z() << ")" << endl;
+    double deltaTime = Time::GetDeltaTime();
 
     for (int i = 0; i < obj_matrixes.size(); i++) {
-        osg::Matrix obj_matrix = obj_matrixes[i]->getMatrix();
+        osg::Matrix obj_matrix = obj_matrixes[i]->getMatrix()->getMatrix();
         osg::Vec3d obj_pos = obj_matrix.getTrans();
 
+        if ((frog_pos.y() == obj_pos.y()) && (frog_pos.x() >= obj_pos.x()-obj_matrixes[i]->getSize()) && (frog_pos.x() <= (obj_pos.x()+ obj_matrixes[i]->getSize()))) {
+            if (frog_pos.x() < -10.0) {
+                frog_pos.x() = -10.0;
+            }
+            else if (frog_pos.x() > 10.0) {
+                frog_pos.x() = 10.0;
+            }
+            else {
+                frog_pos.x() += deltaTime * obj_matrixes[i]->getDirection() * 5; //obj_matrixes[i]->getDirection() * 0.1;
+            }
 
-        if ((frog_pos.y() == obj_pos.y()) && (frog_pos.x() >= obj_pos.x()-0.5) && (frog_pos.x() <= (obj_pos.x()+0.5))) {
-            frog_pos.x() += 0.1;
+            //cout << "zmieniam na obiekcie" << endl;
+            
             _model->setMatrix(
                 osg::Matrixd::scale(_model->getMatrix().getScale()) *
                 osg::Matrixd::rotate(osg::DegreesToRadians(-90.0), osg::Vec3(1, 0, 0)) *
                 osg::Matrixd::rotate(osg::DegreesToRadians(180.0), osg::Vec3(0, 0, 1)) *
                 osg::Matrixd::translate(frog_pos)
             );
-        }
+        } 
 
-
-        cout << "OBJ: (" << obj_pos.x() << ", " << obj_pos.y() << ", " << obj_pos.z() << ")" << endl;
+        //cout << "OBJ: (" << obj_pos.x() << ", " << obj_pos.y() << ", " << obj_pos.z() << ")" << endl;
     }
-
-
-    //osg::Vec3d position = matrix.getTrans();
-    //cout << "FROGG: (" << position.x() << ", " << position.y() << ", " << position.z() << ")" << endl;
 
     return false;
 }
@@ -133,14 +153,16 @@ bool ModelController::handle(const osgGA::GUIEventAdapter& ea,
 class ObjController : public osgGA::GUIEventHandler
 {
 public:
-    ObjController(osg::MatrixTransform* node, int k, string type)
-        : _model(node), k(k), type(type) {}
+    ObjController(RiverObject* obj, string type)//osg::MatrixTransform* node, int k, string type)
+        : _obj(obj), type(type) {}
+        //: _model(node), k(k), type(type) {}
 
     virtual bool handle(const osgGA::GUIEventAdapter& ea,
         osgGA::GUIActionAdapter& aa) override;
 protected:
-    osg::ref_ptr<osg::MatrixTransform> _model;
-    int k;
+    RiverObject* _obj;
+    //osg::ref_ptr<osg::MatrixTransform> _model;
+    //int k;
     string type;
 };
 
@@ -151,42 +173,42 @@ bool ObjController::handle(const osgGA::GUIEventAdapter& ea,
     if (ea.getEventType() != osgGA::GUIEventAdapter::FRAME) return false;
 
     double deltaTime = Time::GetDeltaTime();
-    osg::Matrix matrix = _model->getMatrix();
+    osg::Matrix matrix = _obj->getMatrix()->getMatrix();
     osg::Vec3 position = matrix.getTrans();
 
 
     if (type == "leaf") {
         if (position.x() < -10.0) {
             position.x() = -10.0;
-            k = 1;
+            _obj->setDirection(1);
         }
         else if (position.x() > 10.0) {
             position.x() = 10.0;
-            k = -1;
+            _obj->setDirection(-1); //k = -1;
         }
     }
     else {
         if (position.x() < -9.0) {
             position.x() = -9.0;
-            k = 1;
+            _obj->setDirection(1); //k = 1;
         }
         else if (position.x() > 9.0) {
             position.x() = 9.0;
-            k = -1;
+            _obj->setDirection(-1); //k = -1;
         }
     }
      
-    position.x() += k * 0.1; //Time::GetTime() * deltaTime * 0.3 * k;
+    position.x() +=  deltaTime * _obj->getDirection() * 5; //_obj->getDirection() * 0.1; 
 
     if (type == "leaf") {
-        _model->setMatrix(
-            osg::Matrixd::scale(_model->getMatrix().getScale()) *
-            osg::Matrixd::rotate(_model->getMatrix().getRotate()) *
+        _obj->getMatrix()->setMatrix(
+            osg::Matrixd::scale(_obj->getMatrix()->getMatrix().getScale()) *
+            osg::Matrixd::rotate(_obj->getMatrix()->getMatrix().getRotate()) *
             osg::Matrixd::translate(position)
         );
     }
     else if (type == "planks") {
-        _model->setMatrix(
+        _obj->getMatrix()->setMatrix(
             osg::Matrix::scale(0.015, 0.015, 0.04) *
             osg::Matrix::rotate(osg::DegreesToRadians(90.0), osg::Vec3(0, 0, 1)) *
             osg::Matrix::rotate(osg::DegreesToRadians(90.0), osg::Vec3(0, 1, 0)) *
@@ -202,7 +224,7 @@ bool ObjController::handle(const osgGA::GUIEventAdapter& ea,
 
 
 
-osg::Node* createTable()
+osg::Node* createTable(string file_name)
 {
     using osg::Vec2;
     using osg::Vec3;
@@ -269,7 +291,7 @@ osg::Node* createTable()
     texcoord->push_back(Vec2{ 0.0, 1.0 });
     texcoord->push_back(Vec2{ 1.0, 1.0 });
 
-    osg::ref_ptr<osg::Image> image = osgDB::readImageFile("assets/river.jpg");
+    osg::ref_ptr<osg::Image> image = osgDB::readImageFile(file_name);
     if (image == nullptr) {
         throw std::runtime_error("Couldn't read texture file!");
     }
@@ -325,12 +347,25 @@ osg::Node* stworz_scene(osgViewer::Viewer* viewer)
         osg::StateAttribute::ON);
 
     {
-        auto river = createTable();
+        auto river = createTable("assets/river.jpg");
         osg::MatrixTransform* t = new osg::MatrixTransform();
-        t->setMatrix(osg::Matrix::scale(40, 9.0, 0.2) *
+        t->setMatrix(osg::Matrix::scale(40, 11.0, 0.2) *
             osg::Matrix::translate(-20.0, 0.5, 0.0));
         t->addChild(river);
         scn->addChild(t);
+
+        auto grass = createTable("assets/grass.jpg");
+        osg::MatrixTransform* g = new osg::MatrixTransform();
+        g->setMatrix(osg::Matrix::scale(40, 9.0, 0.2) *
+            osg::Matrix::translate(-20.0, -8.5, 0.1));
+        g->addChild(grass);
+        scn->addChild(g);
+
+        auto* g2 = new osg::MatrixTransform();
+        g2->setMatrix(osg::Matrix::scale(40, 20.0, 0.2) *
+            osg::Matrix::translate(-20.0, 11.5, 0.1));
+        g2->addChild(grass);
+        scn->addChild(g2);
     }
 
     {
@@ -368,6 +403,12 @@ osg::Node* stworz_scene(osgViewer::Viewer* viewer)
         l2->setMatrix(osg::Matrix::scale(0.01, 0.015, 0.01) * osg::Matrix::translate(-4.0, 6.0, 1.0));
         l2->addChild(leaf);
         scn->addChild(l2);
+        
+        // LIŚĆ 3
+        auto* l3 = new osg::MatrixTransform();
+        l3->setMatrix(osg::Matrix::scale(0.01, 0.015, 0.01) * osg::Matrix::translate(9.0, 10.0, 1.0));
+        l3->addChild(leaf);
+        scn->addChild(l3);
 
         // PALETY
         osg::Node* planks = osgDB::readNodeFile("assets/Old_planks.obj");
@@ -442,30 +483,46 @@ osg::Node* stworz_scene(osgViewer::Viewer* viewer)
         r8->addChild(rock);
         scn->addChild(r8);
 
+        auto* r9 = new osg::MatrixTransform();
+        r9->setMatrix(osg::Matrix::scale(0.004, 0.004, 0.004) * osg::Matrix::translate(-12.0, 10.0, 0.0));
+        r9->addChild(rock);
+        scn->addChild(r9);
 
-        osg::ref_ptr<ObjController> controller_l = new ObjController(l, 1, "leaf");
+        auto* r10 = new osg::MatrixTransform();
+        r10->setMatrix(osg::Matrix::scale(0.004, 0.004, 0.004) * osg::Matrix::translate(12.0, 10.0, 0.0));
+        r10->addChild(rock);
+        scn->addChild(r10);
+
+
+        RiverObject* leaf_obj = new RiverObject(l, 1, 0.5);
+        RiverObject* leaf2_obj = new RiverObject(l2, -1, 0.5);
+        RiverObject* plank_obj = new RiverObject(p, 1, 2);
+        RiverObject* plank2_obj = new RiverObject(p2, -1, 2);
+        RiverObject* leaf3_obj = new RiverObject(l3, 1, 0.5);
+
+
+        osg::ref_ptr<ObjController> controller_l = new ObjController(leaf_obj, "leaf"); //new ObjController(l, 1, "leaf");
         viewer->addEventHandler(controller_l);
 
-        osg::ref_ptr<ObjController> controller_l2 = new ObjController(l2, -1, "leaf");
+        osg::ref_ptr<ObjController> controller_l2 = new ObjController(leaf2_obj, "leaf");
         viewer->addEventHandler(controller_l2);
 
-        osg::ref_ptr<ObjController> controller_p = new ObjController(p, -1, "planks");
+        osg::ref_ptr<ObjController> controller_p = new ObjController(plank_obj, "planks");
         viewer->addEventHandler(controller_p);
 
-        osg::ref_ptr<ObjController> controller_p2 = new ObjController(p2, 1, "planks");
+        osg::ref_ptr<ObjController> controller_p2 = new ObjController(plank2_obj, "planks");
         viewer->addEventHandler(controller_p2);
 
+        osg::ref_ptr<ObjController> controller_l3 = new ObjController(leaf3_obj, "leaf");
+        viewer->addEventHandler(controller_l3);
 
-        RiverObject leaf_obj(l ,1, 1);
-        RiverObject leaf2_obj(l2 ,1, 1);
-        RiverObject plank_obj(p ,1, 1);
-        RiverObject plank2_obj(p ,1, 1);
 
-        vector<osg::MatrixTransform*> obj_controllers;
-        obj_controllers.push_back(l);
-        obj_controllers.push_back(p2);
-        obj_controllers.push_back(l2);
-        obj_controllers.push_back(p);
+        vector<RiverObject*> obj_controllers;
+        obj_controllers.push_back(leaf_obj);
+        obj_controllers.push_back(plank2_obj);
+        obj_controllers.push_back(leaf2_obj);
+        obj_controllers.push_back(plank_obj);
+        obj_controllers.push_back(leaf3_obj);
         
 
 
@@ -488,6 +545,8 @@ osg::Node* stworz_scene(osgViewer::Viewer* viewer)
 int main()
 { 
     osgViewer::Viewer viewer;
+
+    //camera->setViewMatrixAsLookAt(osg::Vec3d(10, 10, 10), osg::Vec3d(0, 10, 0), osg::Vec3d(0, 0, 1));
  
     osg::ref_ptr<TimeManager> tm = new TimeManager(&viewer);
     viewer.addEventHandler(tm);
@@ -501,6 +560,9 @@ int main()
     viewer.setSceneData(scn);
     auto om = new osgGA::OrbitManipulator();
     viewer.setCameraManipulator(om);
+
+    viewer.getCamera()->setViewMatrixAsLookAt(osg::Vec3d(10, 10, 10), osg::Vec3d(0, 10, 0), osg::Vec3d(0, 0, 1));
+
 
     viewer.realize();
     while (!viewer.done()) {
